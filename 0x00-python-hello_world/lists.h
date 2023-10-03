e62b15247849c78bb0f0837aad9333507b5eb0a9
Edwin Mwangi <edwindugire@gmail.com>
2023-10-02 21:36:19 -0700

    Task 2

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,18 +3,25 @@
 
 #include <stdlib.h>
 
-// Define a structure for a singly linked list node
+/**
+ * Structure definition for a singly linked list node.
+ * @n: An integer value stored in the node.
+ * @next: A pointer to the next node in the list.
+ *
+ * Description: This structure defines the node of a singly linked list
+ * for a Holberton project.
+ */
 typedef struct listint_s
 {
-	int n;                  // Integer data
-	struct listint_s *next; // Pointer to the next node
+    int n;
+    struct listint_s *next;
 } listint_t;
 
-// Function prototypes
-size_t print_listint(const listint_t *h);                // Print the elements of a linked list
-listint_t *add_nodeint(listint_t **head, const int n);   // Add a new node to the beginning of the linked list
-void free_listint(listint_t *head);                     // Free the memory allocated for a linked list
-int check_cycle(listint_t *list);                       // Check if there is a cycle in the linked list
+/* Function prototypes */
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
 
